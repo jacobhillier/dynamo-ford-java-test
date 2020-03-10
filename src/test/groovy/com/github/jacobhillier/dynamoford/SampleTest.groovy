042350fa3205c5ba0030a9eb66e909de38ba041b
Jacob <jacobhillier83@gmail.com>
2020-03-10 19:39:49 +0000

    added spock and sample test

---
+++ src/test/groovy/com/github/jacobhillier/dynamoford/SampleTest.groovy
@@ -0,0 +1,11 @@
+package com.github.jacobhillier.dynamoford
+
+import spock.lang.Specification
+
+class SampleTest extends Specification {
+
+    void "sample spock test"() {
+        expect:
+        true
+    }
+}
