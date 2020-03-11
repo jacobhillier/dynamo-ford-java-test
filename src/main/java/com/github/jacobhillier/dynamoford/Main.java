eea3c758a2677bc7ef83d55d8b630ed579499cea
Jacob <jacobhillier83@gmail.com>
2020-03-11 18:48:11 +0000

    added lombok

--- src/main/java/com/github/jacobhillier/dynamoford/Main.java
+++ src/main/java/com/github/jacobhillier/dynamoford/Main.java
@@ -1,19 +1,16 @@
 package com.github.jacobhillier.dynamoford;
 
 import com.github.jacobhillier.dynamoford.service.BasketCalculationService;
+import lombok.RequiredArgsConstructor;
 import org.apache.commons.cli.ParseException;
 
 import java.math.BigDecimal;
 
+@RequiredArgsConstructor
 public class Main {
 
     private final ArgumentParser argumentParser;
     private final BasketCalculationService basketCalculationService;
-
-    public Main(ArgumentParser argumentParser, BasketCalculationService basketCalculationService) {
-        this.argumentParser = argumentParser;
-        this.basketCalculationService = basketCalculationService;
-    }
 
     public BigDecimal parseArgsAndCalculate(String[] args) throws ParseException {
         ParsedArguments parsedArguments = argumentParser.parse(args);
--- src/main/java/com/github/jacobhillier/dynamoford/ParsedArguments.java
+++ src/main/java/com/github/jacobhillier/dynamoford/ParsedArguments.java
@@ -1,39 +1,14 @@
 package com.github.jacobhillier.dynamoford;
 
 import com.github.jacobhillier.dynamoford.model.BasketItem;
+import lombok.Data;
 
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Objects;
 
+@Data
 public class ParsedArguments {
 
     private final List<BasketItem> basketItems;
     private final LocalDateTime dateTime;
-
-    public ParsedArguments(List<BasketItem> basketItems, LocalDateTime dateTime) {
-        this.basketItems = basketItems;
-        this.dateTime = dateTime;
-    }
-
-    public List<BasketItem> getBasketItems() {
-        return basketItems;
-    }
-
-    public LocalDateTime getDateTime() {
-        return dateTime;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        ParsedArguments that = (ParsedArguments) o;
-        return Objects.equals(basketItems, that.basketItems) && Objects.equals(dateTime, that.dateTime);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(basketItems, dateTime);
-    }
 }
